d6678662186e01ae6a287bbe7a26217a9715f197
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-10-15 18:52:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const axios = require('axios');
+const yts = require('yt-search');
+
+const app = express();
+const port = 3000;
+
+app.get('/video', async (req, res) => {
+    const searchQuery = req.query.search;
+
+    if (!searchQuery) {
+        return res.status(400).json({ error: 'Search query is required' });
+    }
+
+    try {
+        const videoSearchUrl = `https://api-nako-choru-production.up.railway.app/yt?search=${searchQuery}&limit=1`;
+
+        const videoResponse = await axios.get(videoSearchUrl);
+        const videoData = videoResponse.data[0];
+
+        if (!videoData) {
+            return res.status(404).json({ error: 'Video not found' });
+        }
+
+        const videoUrl = videoData.url;
+        const videoId = new URL(videoUrl).searchParams.get('v');
+
+        const video = await yts({ videoId });
+        const result = {
+            url: `https://youtu.be/${videoId}?si=wLIhI3mr1YV0gl9L`
+        };
+
+        const downloadUrl = `https://betadash-api-swordslush.vercel.app/ytdl?url=${result.url}`;
+
+        const downloadResponse = await axios.get(downloadUrl);
+        const downloadResult = downloadResponse.data.data;
+
+        const videoResult = {
+            title: downloadResult.title,
+            downloadUrl: downloadResult.video,
+            time: videoData.time,
+            views: videoData.views,
+            audio: downloadResult.audio,
+            quality: downloadResult.quality,
+            channelName: videoData.channelName
+        };
+
+        res.json(videoResult);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching video or download URL' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
